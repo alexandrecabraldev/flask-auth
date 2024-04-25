89fb0258b25600ecc2aabdbe1c9009b0853855c9
Alexandre Cabral <alexandre_c.m@hotmail.com>
2024-04-25 10:34:05 -0300

    import database.py in app.py and initialize db

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from models.user import User
+from database import db
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "my_secret_key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+
+db.init_app(app)
+
+# @app.post('/login')
+# def login():
+    
+#     pass
+
+@app.get("/")
+def hello_world():
+    return "hello world"
+
+if __name__ == "__main__":
+    app.run(debug=True)
